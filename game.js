bfd3394411302671b0fdea9a0dac1cd8aa45e748
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-28 10:48:15 +0300

    -

--- game.js
+++ game.js
@@ -173,8 +173,6 @@ window.onload = function () {
 
 
     game.load.spritesheet('badge', 'badge.png', 42, 54);
-    game.load.spritesheet('poison', 'poison.png', 32, 32);
-    game.load.spritesheet('star', 'star.png', 32, 32);
   }
 
   //timer
