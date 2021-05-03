e5f413e78a734ed878d3e93ef1b99d90f567e73b
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-05-03 13:30:36 +0300

    Update game.js

--- game.js
+++ game.js
@@ -125,14 +125,12 @@ function itemHandler(player, item) {
   if (currentScore >= winningScore) {
     game.add.sprite(137, 456, 'gamemachine');
 createBadge();
-items.killAndHide(item);
   }
 }
 
 // when the player collects the badge at the end of the game
 function badgeHandler(player, badge) {
   badge.kill();
-
   var btn = document.getElementById('btn-next');
   btn.removeAttribute("disabled");
   btn.style.border = "2px solid #000";
