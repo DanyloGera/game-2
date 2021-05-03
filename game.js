300e5fe9cf799d764dce553be9f31633d70c8534
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-05-03 12:45:37 +0300

    Update game.js

--- game.js
+++ game.js
@@ -132,16 +132,14 @@ createBadge();
 // when the player collects the badge at the end of the game
 function badgeHandler(player, badge) {
   badge.kill();
-  itemKiller();
+  items.remove(item);
   var btn = document.getElementById('btn-next');
   btn.removeAttribute("disabled");
   btn.style.border = "2px solid #000";
   won = true;
 
 }
-function itemKiller(items) {
-  items.destroy();
-}
+
 // setup game when the web page loads
 window.onload = function () {
   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
